ab9edfcd4ffc59026b84fee451d3d795d41ce3ba
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-11-17 18:54:44 +0900

    Fix: レンダリングを修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), solid({ ssr: true })],
+  plugins: [tsconfigPaths(), solid()],
 })
