c36d1c9e97c1ca1db789cb395fe206cf4c604981
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-12-23 13:48:54 +0900

    feat: フォントをサブセット化

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import uno from "unocss/vite";
 import vueRouter from "unplugin-vue-router/vite";
+import { fontSubsetter } from "rollup-plugin-font-subsetter";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueRouter(), uno()],
+  plugins: [vue(), vueRouter(), uno(), fontSubsetter()],
   server: {
     allowedHosts: true,
   },
